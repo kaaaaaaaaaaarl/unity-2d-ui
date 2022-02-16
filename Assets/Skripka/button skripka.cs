9ee904d819c0e61300c4b94e8928e02a7b180548
kaaaaaaaaaaarl <caalis18@gmail.com>
2022-02-16 14:15:50 +0200

    pirmais skripka

---
+++ Assets/Skripka/button skripka.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonskripka : MonoBehaviour {
+	public string teksts;
+
+	// Use this for initialization
+	public GameObject tekstlauk;
+	public GameObject tekstlauklauk;
+	public void darbatext(){
+		teksts = tekstlauk.GetComponent<Text> ().text;
+		tekstlauklauk.GetComponent<Text> ().text = "sveiks" + teksts.ToUpper () + "!";
+	}
+}
+
