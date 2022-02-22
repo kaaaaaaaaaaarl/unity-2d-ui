a53fb5416379f69401348ffee6b4bd24b448fe2b
kaaaaaaaaaaarl <caalis18@gmail.com>
2022-02-22 15:24:33 +0200

    for reserch

---
+++ Assets/Skripka/kursor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kursor : MonoBehaviour {
+	public AudioSource skanasAvots;
+	public AudioClip skana;
+	// Use this for initialization
+	public void nobiditsUzAttela () {
+		skanasAvots.PlayOneShot (skana);
+
+}
+	public void nobiditsPromNoAttela ()
+	{
+		skanasAvots.Stop();
+	}
+}
---
+++ Assets/Skripka/objectmove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystem;
+
+
+	public class ObjectuParvietosana : monoBehavior, IPointerDownHandler, IBeginDragHolder, IDragHandler, IEndDragHandler{
+	private RectTransform transformacijasLogs  {
+		public Canvas kanvas;
+	}
+
+	// Use this for initialization
+	void start(){
+		transformacijasLogs = getComponent<RectTransform> ();
+	}
+	public void OnPointerDown(pointerEventData notikums){
+		Debug.Log ("kreisais uz objekta");
+	}
+	public void OnBeginDrag(pointerEventData notikums){
+		Debug.Log ("uz objekta vilksanu");
+	}
+	public void OnDrag(pointerEventData notikums){
+		TransformacijasLogs.anchoredPosition += notikums.delta / kanva.scaleFaktor;
+	}
+	public void OnEndDrag(pointerEventData notikums){
+		Debug.Log ("obj vilksana end");
+	}
+}
